f8ea97f0be89efa35e43de859f7ed528edb7ffb2
kowoohyuk <kowoohyuk91@gmail.com>
2020-06-02 23:11:16 +0900

    init

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello!!!</Text>
+      <Text style={styles.subText}>React Native</Text>
     </View>
   );
 }
@@ -12,8 +13,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ffcc00",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 32,
+    marginBottom: "20%",
+    color: "#fff",
+  },
+  subText: {
+    fontSize: 20,
+    color: "#fff",
   },
 });
---
+++ README.md
@@ -0,0 +1,3 @@
+# nomad rn weather
+
+노마드코더 리액트네이티브 날씨 앱
